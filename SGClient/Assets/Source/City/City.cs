71127148a9c8558ceeb1de058d0d7db8a4b80c17
Gamesjiazhi <116024141>
2017-06-17 08:07:13 +0800

    city

---
+++ SGClient/Assets/Source/City/City.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class City : MonoBehaviour 
+{
+	public Transform	_buildings;
+    public Camera2D     _camera;
+    
+	void Start()
+	{
+		
+	}
+
+	public void SelectBuilding( Transform building )
+	{
+        LuaFun.buildingselect.Call( building );
+    }  
+}
