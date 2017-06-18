1154660f3e2dd25a1516b2aa036b7da0a3d65c7a
weizuojiazhi <245555162@qq.com>
2017-06-18 20:31:39 +0800

    update

---
+++ SGClient/Assets/Source/Base/SequenceActive.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceActive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	protected void OnEnable()
+	{
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild (i).gameObject.SetActive(false);
+		}
+		StartCoroutine( Execute() );
+	}
+
+	protected void OnDisable()
+	{
+		StopCoroutine( Execute() );
+	}
+
+	IEnumerator Execute()
+	{
+		for ( int i = 0; i < transform.childCount; i++ )
+		{
+			transform.GetChild (i).gameObject.SetActive(true);
+			yield return 0;
+		}
+	}
+}
