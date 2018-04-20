541f527bdf461c80088916a97b25300b39c6ce86
weizuojiazhi <jiazhi314@126.com>
2018-04-20 08:26:49 +0800

    update

--- LKAutoTools/PublishSQL/CompareData/dev_compare_test.bat
+++ LKAutoTools/PublishSQL/CompareData/dev_compare_test.bat
@@ -1,1 +1,1 @@
-dbcomp.exe "119.29.56.213|zaya|zaya|119.29.56.213|zaya|zaya|sg_dev_data|sg_test_data"
+dbcomp.exe "119.29.56.213|zaya|4159217|119.29.56.213|zaya|4159217|sg_dev_data|sg_test_data"
--- LKAutoTools/PublishSQL/CompareData/test_compare_online.bat
+++ LKAutoTools/PublishSQL/CompareData/test_compare_online.bat
@@ -1,1 +1,1 @@
-dbcomp.exe "119.29.56.213|zaya|zaya|39.108.103.129|zaya|zaya|sg_test_data|sg_data"
+dbcomp.exe "119.29.56.213|zaya|4159217|39.108.103.129|zaya|zaya|sg_test_data|sg_data"
