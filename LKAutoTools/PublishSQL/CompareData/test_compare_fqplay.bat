565977270bc114b8bb8e111e522080f57549490e
jiazhi <116024141@qq.com>
2018-11-12 19:42:54 +0800

    update

---
+++ LKAutoTools/PublishSQL/CompareData/test_compare_fqplay.bat
@@ -0,0 +1,1 @@
+dbcomp.exe "119.29.56.213|eye|eye|47.107.228.49|eye|eye123,./|sg_test_data|three_data"
---
+++ LKAutoTools/PublishSQL/CompareGame/test_check_fqplay.bat
@@ -0,0 +1,1 @@
+checktab.exe "119.29.56.213|eye|eye|47.107.228.49|eye|eye123,./|sg_test_game|three_game"
